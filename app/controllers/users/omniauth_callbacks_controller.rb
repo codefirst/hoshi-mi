c97ab1ffec56fa4049de4170a5c32c94fd6bc150
mallowlabs <mallowlabs@gmail.com>
2012-09-01 05:28:01 +0900

    Added: omniauth_callbacks_controller

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,2 +1,7 @@
 class Users::OmniauthCallbacksController < ApplicationController
+  def twitter
+    uid = env["omniauth.auth"]["uid"]
+    user = User.find_by_uid(uid) || User.create!(:uid => uid, :name => env["omniauth.auth"]["info"]["nickname"])
+    sign_in_and_redirect user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 HoshiMi::Application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" } do
+    get '/users/sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
