985eef504a947a7aa55a9eb34448b25eea24edf1
mallowlabs <mallowlabs@gmail.com>
2012-09-01 04:47:00 +0900

    rails new hoshi-mi -J -T -d postgresql --skip-bundle

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HoshiMi::Application.config.secret_token = '7eabc24a6d592fc6cf0ba9779035ef3e5676e5b180278edae5f17b71b14faabc23200f052ce395325d0e7293798821c56559e09ba7039670eafe18508250295f'
