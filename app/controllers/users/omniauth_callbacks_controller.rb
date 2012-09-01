9b75e22a4c95a1c3db50f6e8e8b60b4c5ac137c1
mallowlabs <mallowlabs@gmail.com>
2012-09-01 09:36:46 +0900

    Added: login link

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,7 +1,8 @@
 class Users::OmniauthCallbacksController < ApplicationController
   def twitter
     uid = env["omniauth.auth"]["uid"]
-    user = User.find_by_uid(uid) || User.create!(:uid => uid, :name => env["omniauth.auth"]["info"]["nickname"])
+    name = env["omniauth.auth"]["info"]["nickname"]
+    user = User.find_by_uid(uid) || User.create!(:uid => uid, :name => name)
     sign_in_and_redirect user
   end
 end
