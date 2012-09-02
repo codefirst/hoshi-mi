390d91eb208b20fc5e27ce3af799a6ec05a2cec6
mallowlabs <mallowlabs@gmail.com>
2012-09-02 12:44:12 +0900

    Added: twitter icon

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,7 +2,10 @@ class Users::OmniauthCallbacksController < ApplicationController
   def twitter
     uid = env["omniauth.auth"]["uid"]
     name = env["omniauth.auth"]["info"]["nickname"]
+    icon_url = env["omniauth.auth"]["info"]["image"]
     user = User.find_by_uid(uid) || User.create!(:uid => uid, :name => name)
+    user.icon_url = icon_url
+    user.save!
     sign_in_and_redirect user
   end
 end
