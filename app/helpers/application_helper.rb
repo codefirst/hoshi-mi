2d65bfd78f64651ae83da8d16e1ded369b767aa4
mallowlabs <mallowlabs@gmail.com>
2012-09-05 23:34:13 +0900

    Refactored: user_link refs #1302

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def user_link(user)
+    return '' if user.nil?
+    link_to image_tag(user.icon_url, {:class => "profile"}) + ' ' + user.name, "https://twitter.com/#{user.name}", {:target => :blank}
+  end
 end
