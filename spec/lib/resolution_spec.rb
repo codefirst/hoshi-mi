a7c554836f7a6144fbcce71e29e11fc9ef43b26f
mzp <mzpppp@gmail.com>
2012-11-14 20:42:57 +0900

    add test case for multi-resolution

---
+++ spec/lib/resolution_spec.rb
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe HoshiMi::Resolution do
+  before do
+    @time = Time.now
+  end
+
+  describe "hour" do
+    before do
+      @resolution = HoshiMi::Resolution.hour
+    end
+
+    context "begin" do
+      subject { @resolution.beginning_of @time }
+      it { should == @time.change(:min => 0) }
+    end
+
+    context "end" do
+      subject { @resolution.end_of @time }
+      it { should == @time.change(:min => 59) }
+    end
+  end
+
+  describe "day" do
+    before do
+      @resolution = HoshiMi::Resolution.day
+    end
+
+    context "begin" do
+      subject { @resolution.beginning_of @time }
+      it { should == @time.beginning_of_day }
+    end
+
+    context "end" do
+      subject { @resolution.end_of @time }
+      it { should == @time.end_of_day }
+    end
+  end
+
+  describe "month" do
+    before do
+      @resolution = HoshiMi::Resolution.month
+    end
+
+    context "begin" do
+      subject { @resolution.beginning_of @time }
+      it { should == @time.beginning_of_month }
+    end
+
+    context "end" do
+      subject { @resolution.end_of @time }
+      it { should == @time.end_of_month }
+    end
+  end
+
+  describe "comparable" do
+    subject { HoshiMi::Resolution.day }
+    it { should < HoshiMi::Resolution.month }
+    it { should == HoshiMi::Resolution.day }
+    it { should > HoshiMi::Resolution.hour }
+  end
+
+  describe "default value" do
+    context "specified" do
+      before do
+        hoshi_mi_resolution("hour") {
+          @resolution = HoshiMi::Resolution.default
+        }
+      end
+
+      subject { @resolution }
+      it { should == HoshiMi::Resolution.hour }
+    end
+
+    context "not specified" do
+      before do
+        hoshi_mi_resolution(nil) {
+          @resolution = HoshiMi::Resolution.default
+        }
+      end
+
+      subject { @resolution }
+      it { should == HoshiMi::Resolution.day }
+    end
+  end
+end
--- spec/models/graph_spec.rb
+++ spec/models/graph_spec.rb
@@ -3,12 +3,13 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Graph do
   before do
-    @graph = Graph.new
+    @graph = Graph.new(:service => "graph-test", :section => "foo", :name => "bar")
     @user = User.new
     @user.id = 1
     @graph.created_by = @user
     @other = User.new
     @other.id = 2
+    @graph.save!
   end
   context "my graph" do
     subject { @graph.owner?(@user) }
@@ -28,5 +29,41 @@ describe Graph do
   context "user not seved" do
     subject { @graph.owner?(User.new) }
     it { should == false }
+  end
+
+
+  describe "logs" do
+    before do
+      @time = Time.now.utc.beginning_of_month
+      [
+        [ @time, 1 ],
+        [ @time + 1.minute, 2],
+        [ @time + 1.hour, 3],
+        [ @time + 1.day, 4],
+        [ @time + 1.month, 5]
+      ].each do|happened_at, number|
+        log = Log.new(:happened_at => happened_at, :number => number)
+        log.graph = @graph
+        log.save!
+      end
+    end
+
+    context "hour" do
+      subject { @graph.logs_by HoshiMi::Resolution.hour }
+      it { should have(4).items }
+      its(:first) { should  == [@time, 1.5] }
+    end
+
+    context "day" do
+      subject { @graph.logs_by HoshiMi::Resolution.day }
+      it { should have(3).items }
+      its(:first) { should  == [@time, 2] }
+    end
+
+    context "month" do
+      subject { @graph.logs_by HoshiMi::Resolution.month }
+      it { should have(2).items }
+      its(:first) { should  == [@time, 2.5] }
+    end
   end
 end
