e6ddc5074ac0de25bbd3884c00d8b52bbd72fd8f
mzp <mzpppp@gmail.com>
2014-02-21 09:30:19 +0000

    fix to deploy on heroku

--- config/application.rb
+++ config/application.rb
@@ -66,5 +66,8 @@ module HoshiMi
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # disable initialize on precomplie
+    config.assets.initialize_on_precompile = false
   end
 end
